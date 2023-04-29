66043e4dc14998457ba79ce2b1a94ec8f034c867
静默 <cau1403090523@gmail.com>
2023-04-30 02:31:02 +0800

    Update Main.java

--- demo/src/main/java/org/hzz/Main.java
+++ demo/src/main/java/org/hzz/Main.java
@@ -1,10 +1,9 @@
 package org.hzz;
 
-import lombok.extern.slf4j.Slf4j;
 
 public class Main {
     public static void main(String[] args) {
         MyFactory.getSomething();
-        System.out.println("over"); // problem:not printed
+        System.out.println("over");
     }
 }
